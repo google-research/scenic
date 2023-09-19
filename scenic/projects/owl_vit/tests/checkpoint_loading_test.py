bca938abdc118eff96d6354aab9a6fab8c6fd4e7
Matthias Minderer <mjlm@google.com>
2023-09-19 06:01:07 -0700

    Add test for checkpoint loading.
    PiperOrigin-RevId: 566606641

---
+++ scenic/projects/owl_vit/tests/checkpoint_loading_test.py
@@ -0,0 +1,51 @@
+"""Tests loading of OWL-ViT checkpoints for real configs.."""
+import functools
+import inspect
+from absl.testing import absltest
+from absl.testing import parameterized
+import jax
+import jax.numpy as jnp
+from scenic.projects.owl_vit import configs
+from scenic.projects.owl_vit import models
+
+
+class CheckpointLoadingTest(parameterized.TestCase):
+  """Tests that checkpoints can be loaded."""
+
+  @parameterized.named_parameters(
+      *inspect.getmembers(configs, inspect.ismodule)
+  )
+  def test_checkpoint_loading(self, config_module):
+    """Tests that real checkpoints can be loaded and used with the model."""
+    # We test the canonical checkpoint if there is one:
+    try:
+      config = config_module.get_config(init_mode='canonical_checkpoint')
+    except TypeError:
+      config = config_module.get_config()
+
+    module = models.TextZeroShotDetectionModule(
+        body_configs=config.model.body,
+        normalize=config.model.normalize,
+        box_bias=config.model.box_bias,
+    )
+
+    # Parameter initialization:
+    batch_size = 8
+    img_size = config.dataset_configs.input_size
+    num_queries = 10
+    seq_len = config.dataset_configs.max_query_length
+    images = jnp.ones((batch_size, img_size, img_size, 3))
+    texts = jnp.ones((batch_size, num_queries, seq_len), dtype=jnp.int32)
+    init = functools.partial(module.init, train=False)
+    init_params = jax.eval_shape(init, jax.random.PRNGKey(0), images, texts)
+    params = module.bind({}).load(init_params, config.init_from)
+
+    # Test running the model with the parameters:
+    fn = functools.partial(module.apply, train=False)
+    out = jax.eval_shape(fn, {'params': params}, images, texts)
+
+    self.assertContainsSubset({'pred_boxes', 'pred_logits'}, out.keys())
+
+
+if __name__ == '__main__':
+  absltest.main()
