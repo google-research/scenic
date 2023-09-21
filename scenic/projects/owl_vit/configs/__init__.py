a41317783b2f33f85b94f7af0860a93c4265db58
Matthias Minderer <mjlm@google.com>
2023-09-21 00:24:18 -0700

    Internal.
    PiperOrigin-RevId: 567210131

---
+++ scenic/projects/owl_vit/configs/__init__.py
@@ -0,0 +1,12 @@
+"""Module providing OWL-ViT configs."""
+
+# OWL-ViT v1:
+from scenic.projects.owl_vit.configs import clip_b16
+from scenic.projects.owl_vit.configs import clip_b32
+from scenic.projects.owl_vit.configs import clip_b32_finetune
+from scenic.projects.owl_vit.configs import clip_l14
+from scenic.projects.owl_vit.configs import clip_l14_with_masks
+
+# OWL-ViT v2:
+from scenic.projects.owl_vit.configs import owl_v2_clip_b16
+from scenic.projects.owl_vit.configs import owl_v2_clip_l14
