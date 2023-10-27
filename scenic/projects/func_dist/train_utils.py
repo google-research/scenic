2656e0a0abdd816942c804fc2991dd96c377f00e
Gabriel Dulac-Arnold <dulacarnold@google.com>
2023-10-27 04:46:20 -0700

    Project forked to https://github.com/minttusofia/hold-policies
    PiperOrigin-RevId: 577158333

--- scenic/projects/func_dist/train_utils.py
+++
@@ -1,7 +0,0 @@
-"""Extend scenic.train_lib_deprecated.train_utils with custom datasets."""
-
-from scenic.projects.func_dist.datasets import ssv2_regression  # pylint: disable=unused-import
-from scenic.train_lib_deprecated import train_utils
-
-get_dataset = train_utils.get_dataset
-TrainState = train_utils.TrainState
