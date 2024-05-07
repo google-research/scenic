1dc7352d1dec0ce4e13e2252f50d14082ca373e0
Scenic Authors <no-reply@google.com>
2024-05-07 13:35:11 -0700

    ViViTMultiHeadClassificationModel - Add support for tasks with less than 5 classes.
    PiperOrigin-RevId: 631531927

--- scenic/projects/vivit/model.py
+++ scenic/projects/vivit/model.py
@@ -916,6 +916,9 @@ class ViViTMultiHeadClassificationModel(ViViTClassificationModel):
     """
     del split  # for all splits, we return the same metric functions
 
+    num_classes_in_each_head = (
+        self.dataset_meta_data.get('class_splits', [-1]))
+    minimal_num_classes = min(num_classes_in_each_head)
     def classification_metrics_function(logits, batch, metrics, class_splits,
                                         split_names):
 
@@ -947,18 +950,21 @@ class ViViTMultiHeadClassificationModel(ViViTClassificationModel):
             (model_utils.joint_accuracy(logits, one_hot_targets, class_splits,
                                         weights),
              base_model_utils.num_examples(logits, one_hot_targets, weights)))
-        pairwise_top_five = base_model_utils.psum_metric_normalizer(
-            (model_utils.joint_top_k(
-                logits, one_hot_targets, class_splits, k=5, weights=weights),
-             base_model_utils.num_examples(logits, one_hot_targets, weights)))
         eval_name = f'{split_names[0]}-{split_names[1]}'
         evaluated_metrics[f'{eval_name}_accuracy'] = pairwise_acc
-        evaluated_metrics[f'{eval_name}_accuracy_top_5'] = pairwise_top_five
+        if minimal_num_classes > 5:
+          pairwise_top_five = base_model_utils.psum_metric_normalizer(
+              (model_utils.joint_top_k(
+                  logits, one_hot_targets, class_splits, k=5, weights=weights),
+               base_model_utils.num_examples(logits, one_hot_targets, weights)))
+          evaluated_metrics[f'{eval_name}_accuracy_top_5'] = pairwise_top_five
 
       return evaluated_metrics
-
+    metrics = ViViT_CLASSIFICATION_METRICS
+    if minimal_num_classes <= 5:
+      metrics = ViViT_CLASSIFICATION_METRICS_BASIC
     return functools.partial(
         classification_metrics_function,
-        metrics=ViViT_CLASSIFICATION_METRICS,
+        metrics=metrics,
         class_splits=self.class_splits,
         split_names=self.split_names)
