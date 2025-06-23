e4404d294b2cb14cfa92a1021ba43d5cf8c31daa
Scenic Authors <no-reply@google.com>
2025-06-23 15:17:14 -0700

    Migrate `scenic/common_lib/debug_utils` cost-analysis to `roofline`.
    The underlying hlo_module_cost_analysis function is being deprecated.
    
    This also requires a tiny change to `flax_model_apply_fn`; in practice this was already being called by functions that don't accept a `jnp.ndarray`, but pytype never caught the issue since `flax_model_apply_fn` was never called with `**dummy_input` directly.
    
    PiperOrigin-RevId: 774945555

--- scenic/common_lib/debug_utils.py
+++ scenic/common_lib/debug_utils.py
@@ -24,7 +24,6 @@ from typing import Any, Callable, Optional, Sequence, Set, Tuple, Union
 from absl import logging
 from clu import parameter_overview
 import jax
-from jax.experimental import roofline
 import jax.numpy as jnp
 from jax.tree_util import tree_map
 import ml_collections
@@ -136,8 +135,8 @@ def compute_flops(
     else:
       dummy_input.append(None)
 
-  _, analysis = roofline.roofline(flax_model_apply_fn)(*dummy_input)
-  flops = analysis.unfused_flops
+  analysis = jax.jit(flax_model_apply_fn).lower(*dummy_input).cost_analysis()
+  flops = analysis['flops']
   if fuse_multiply_add:
     flops = flops / 2
   logging.info('GFLOPs %0.3f for input spec: %s', flops / 10**9, input_spec)
@@ -145,7 +144,7 @@ def compute_flops(
 
 
 def compute_flops_with_pytree(
-    flax_model_apply_fn: Callable[..., Any],
+    flax_model_apply_fn: Callable[[jnp.ndarray], Any],
     input_spec: PyTree,
     unpack_input: bool = True,
     fuse_multiply_add: bool = True,
@@ -194,16 +193,13 @@ def compute_flops_with_pytree(
   dummy_input = create_dummy_input(input_spec)
 
   if isinstance(dummy_input, dict) and unpack_input:
-    # We can't pass custom kwargs to roofline, so we have to use a lambda.
-    _, analysis = roofline.roofline(
-        lambda: flax_model_apply_fn(**dummy_input)
-    )()
+    analysis = jax.jit(flax_model_apply_fn).lower(**dummy_input).cost_analysis()
   elif isinstance(dummy_input, abc.Sequence) and unpack_input:
-    _, analysis = roofline.roofline(flax_model_apply_fn)(*dummy_input)
+    analysis = jax.jit(flax_model_apply_fn).lower(*dummy_input).cost_analysis()
   else:
-    _, analysis = roofline.roofline(flax_model_apply_fn)(dummy_input)
+    analysis = jax.jit(flax_model_apply_fn).lower(dummy_input).cost_analysis()
 
-  flops = analysis.unfused_flops
+  flops = analysis['flops']
   if fuse_multiply_add:
     flops = flops / 2
   logging.info('GFLOPs %0.3f for input spec: %s', flops / 10**9, input_spec)
